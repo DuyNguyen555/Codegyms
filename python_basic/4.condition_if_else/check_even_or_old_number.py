2a27ad0684caedc8e266518cb97b78b6d984f1b8
DuyNguyen <drduyvn555@gmail.com>
2021-12-14 14:26:00 +0700

    create

--- python_basic/4.condition_if_else/check_even_or_old_number.py
+++
@@ -1,7 +0,0 @@
-num = int(input("Enter the nature number: "))
-if num % 2 == 0:
-    print("There's a even number.")
-elif num % 2 == 1:
-    print("There's a old number.")
-else:
-    print("Not a negative number")
