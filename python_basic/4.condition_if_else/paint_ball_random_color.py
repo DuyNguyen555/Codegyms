99bc8f7f6e8cc4bb5932a2d5865f8a71aa5c594f
DuyNguyen <drduyvn555@gmail.com>
2021-12-18 10:18:30 +0700

    Update paint_ball_random_color.py

--- python_basic/4.condition_if_else/paint_ball_random_color.py
+++ python_basic/4.condition_if_else/paint_ball_random_color.py
@@ -3,7 +3,8 @@ import random
 
 number = int(random.uniform(0,3))
 print(number)
-turtle.bgcolor('skyblue')turtle.title('circle')
+turtle.bgcolor('skyblue')
+turtle.title('circle')
 ball = turtle.Turtle()
 ball.shape('circle')
 ball.color('green') if number < 1 else ball.color('yellow') if number < 2 else ball.color('red') if number < 3 else print("This's no colors")
