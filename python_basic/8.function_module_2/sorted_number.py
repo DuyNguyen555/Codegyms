e1c764d13aa3a6e1498414f73fe222a9c214c0f8
DuyNguyen <drduyvn555@gmail.com>
2021-12-19 16:45:49 +0700

    Update sorted_number.py

--- python_basic/8.function_module_2/sorted_number.py
+++ python_basic/8.function_module_2/sorted_number.py
@@ -1,12 +1,13 @@
 def sort(x, y, z):
     sorted_number = sorted((x, y, z))
-
-    return ' '.join(sorted_number)
+    return sorted_number
 
 def main():
-    x, y ,z = [str(input("Enter 3 number: ")) for _ in range (3)]
+    x, y ,z = (int(input("Enter 3 number: ")) for _ in range (3))
     print('Original number:',x ,y , z)
-    print('Sorted number: ',sort(x, y, z))
+    print('Sorted number:', end = ' ')
+    for i in sort(x, y, z):    
+        print(i, end =" ")
 
 if __name__ == '__main__':
     main()
