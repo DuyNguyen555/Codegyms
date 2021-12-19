362ccb5c418ec9d268244e3372115f46846355bc
DuyNguyen <drduyvn555@gmail.com>
2021-12-19 14:38:23 +0700

    updata

---
+++ python_basic/8.function_module_2/spending_management.py
@@ -0,0 +1,44 @@
+expenses = []
+    
+def add_item(myTempList, item):
+    myTempList.append(item)
+
+def find_index_item(myTempList, item_name):
+    result = -1
+    length = len(myTempList)
+    for i in range(length):
+        if myTempList[i]['name'] == item_name:
+            result = i
+    return result
+
+def remove_item(myTempList, item_name):
+    if find_index_item(myTempList, item_name) > -1:
+        del myTempList[find_index_item(myTempList, item_name)]
+    else:
+        print(item_name + " not in list")
+
+def main():
+    while True:
+        print("What do you want to do? -\n"\
+            "1. Add\n" \
+            "2. Remove")
+        option = int(input("Select option 1 or 2: "))
+        name_input = input("Item name: ")
+
+        if option == 1:
+            cost_input = int(input("Item cost: "))
+            date_input = input("Date: ")
+            item = {'name': name_input, 'cost':cost_input, 'date':date_input}
+            add_item(expenses, item) 
+            print("Your expenses: ", expenses)
+
+        elif option == 2:
+            remove_item(expenses, name_input)
+            print("Your expenses: ", expenses)
+        elif option == 3:
+            break
+        else:
+            print("Invalid input")
+
+if __name__ == '__main__':
+    main()
--- python_basic/8.function_module_2/spending_management/spending_management.py
+++
@@ -1,3 +0,0 @@
-name = []
-spending =[]
-date = []
