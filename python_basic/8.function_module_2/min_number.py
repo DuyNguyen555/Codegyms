dc9e1eabebae3f6e42fe786266cd2c46ceb8b824
DuyNguyen <drduyvn555@gmail.com>
2021-12-19 16:56:05 +0700

    Create min_number.py

---
+++ python_basic/8.function_module_2/min_number.py
@@ -0,0 +1,10 @@
+def Min(num):
+    result = min(num)
+    return result
+
+def main():
+    list_1 = [21, 56, 98, 19, 36, 71, 24, 40]
+    print(Min(list_1))
+    
+if __name__ == '__main__':
+    main()
