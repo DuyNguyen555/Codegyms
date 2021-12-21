31566ef6674bd33f6070984b18c0034d361a86e6
DuyNguyen <drduyvn555@gmail.com>
2021-12-21 17:01:24 +0700

    Update turtle_jog.py

--- python_basic/9.list_tuples/turtle_jog.py
+++ python_basic/9.list_tuples/turtle_jog.py
@@ -1,18 +1,40 @@
 import turtle as t
-import random
-t.setup(800,800,0,0)
+from random import choice
+# Tạo màn hình
+screen = t.Screen()
+screen.setup(1500,300,0,0)
+
+def race_track():
+    race = t.Turtle()
+    race.hideturtle()
+    race.speed(0)
+    position_race = -75
+    for i in range(7):
+        race.penup()
+        race.goto(x = -660, y = position_race + 30*i)
+        race.pendown()
+        # for n in range(2):
+        for j in range(4):
+            race.fd(10)
+            race.lt(90)
+        race.fd(50)
+    race.pensize(5)
+    race.color("red")
+    race.rt(90)
+    race.fd(180)
+    race.rt(180)
 
 def full_turtle():
     # lưu tất cả rùa vào list
     global all_turtle
     all_turtle = []
     # Màu của rùa
-    color_turtle = ['red', 'blue', 'green', 'yellow', 'pink', 'black']
-    # Tốc độ của rùa
+    color_turtle = ['red', 'blue', 'green', 'orange', 'hotpink', 'black']
+    # Khoảng cách mà rùa đi được
     global speed_turtle
-    speed_turtle = [8, 12, 5, 17, 20, 23]
+    speed_turtle = [5, 10, 15, 20, 25, 30]
     # Nơi xuất phát
-    position_x = -350
+    position_x = -650
     position_y = [-60, -30, 0, 30, 60, 90]
 
     for i in range(6):
@@ -26,12 +48,21 @@ def full_turtle():
         turtles.color(color_turtle[i])
         all_turtle.append(turtles)
 
-def SpeedTurtle():
-    
-    pass
+def Turtle_running(turtle):
+    run = True
+    while run:
+        for i in turtle:
+            i.penup()
+            i.fd(choice(speed_turtle))
+            i.pendown()
+            i.fd(choice(speed_turtle))
+        if i.xcor() > 150 :
+            run = False
 
 def main():
+    race_track()
     full_turtle()
+    Turtle_running(all_turtle)
     t.exitonclick()
 
 if __name__ == '__main__':
