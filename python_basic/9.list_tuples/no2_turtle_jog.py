1da39feee097ab52f7e3cce6cb52a72de779df4c
DuyNguyen <drduyvn555@gmail.com>
2021-12-22 20:08:35 +0700

    update

--- python_basic/9.list_tuples/no2_turtle_jog.py
+++ python_basic/9.list_tuples/no2_turtle_jog.py
@@ -1,16 +1,16 @@
 import turtle as t
-import turtle_jog_no1
+import no1_turtle_jog
 
 def TurtleRace():
     global full_turtle, name_turtle, way_turtle
-    turtle_jog_no1.AllTurtle()
+    no1_turtle_jog.AllTurtle()
     # chuyển list của biến all_turtle thành full_turtle
-    full_turtle = turtle_jog_no1.all_turtle
+    full_turtle = no1_turtle_jog.all_turtle
     # Gọi tên rùa theo màu sắc
-    name_turtle = turtle_jog_no1.color_turtle
-    turtle_jog_no1.TurtleRunning(full_turtle)
+    name_turtle = no1_turtle_jog.color_turtle
+    no1_turtle_jog.TurtleRunning(full_turtle)
     # quãng đường rùa đi được
-    way_turtle = turtle_jog_no1.finish
+    way_turtle = no1_turtle_jog.finish
 
 def finish():
     global ranking, way_sort
