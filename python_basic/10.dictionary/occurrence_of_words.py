45ae3dea828964a86849b29fcd91243a5981cb00
DuyNguyen <drduyvn555@gmail.com>
2021-12-25 17:11:49 +0700

    Update occurrence_of_words.py

--- python_basic/10.dictionary/occurrence_of_words.py
+++ python_basic/10.dictionary/occurrence_of_words.py
@@ -40,6 +40,12 @@ def DisplayOccurrence():
 if __name__ == '__main__':
     # Văn bản cho trước
     text = str("""           Các công ty phụ thuộc vào nền tảng dữ liệu để cấu trúc, phát triển và cải tiến doanh
-               nghiệp. Các Data Scientist làm việc với các con số, phân tích một một khối lượng lớn Data để xuất ra những Insight ý nghĩa. Những insight này rất hữu ích khi phân tích công ty và các hoạt động của công ty trên thị trường từ đó đưa ra các quyết định đúng đắn. Cũng như các ngành công nghiệp thương mại khác, ngành chăm sóc sức khỏe cũng ứng dụng Data Science. Nơi mà công nghệ đang có nhu cầu rất lớn để nhận dạng các khối u siêu nhỏ ngay từ giai đoạn đầu. Thống kê chỉ ra số lượng vai trò của các Data Scientist đã tăng trưởng 650% kể từ năm 2012. Khoảng 11,5 triệu việc làm liên quan đến chức danh này sẽ được tạo ra đến năm 2026""")
+               nghiệp. Các Data Scientist làm việc với các con số, phân tích một một khối lượng lớn Data để xuất ra
+               những Insight ý nghĩa. Những insight này rất hữu ích khi phân tích công ty và các hoạt động của công ty
+               trên thị trường từ đó đưa ra các quyết định đúng đắn. Cũng như các ngành công nghiệp thương mại khác
+               ngành chăm sóc sức khỏe cũng ứng dụng Data Science. Nơi mà công nghệ đang có nhu cầu rất lớn để nhận
+               dạng các khối u siêu nhỏ ngay từ giai đoạn đầu. Thống kê chỉ ra số lượng vai trò của các Data Scientist
+               đã tăng trưởng 650% kể từ năm 2012. Khoảng 11,5 triệu việc làm liên quan đến chức danh này sẽ được tạo
+               ra đến năm 2026""")
 
     DisplayOccurrence()
