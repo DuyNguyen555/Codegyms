22553eaab8de80875bc17eb9d7fbb5a6058240a2
DuyNguyen <drduyvn555@gmail.com>
2022-01-13 09:35:18 +0700

    Create linked_list(2).py

---
+++ python_basic/09.list_tuples/linked_list(2).py
@@ -0,0 +1,47 @@
+class Node:
+    def __init__(self, num = None):
+        self.data = num
+        self.next = None
+
+class linked_list:
+    def __init__(self):
+        self.head = None
+    
+    def append_begin_list(self, new_num):
+        new_node = Node(new_num)
+        new_node.next = self.head
+        self.head = new_node
+    
+    def append_end_list(self, new_num):
+        new_node = Node(new_num)
+        if self.head is None:
+            new_node = self.head
+            return
+        num_next = self.head
+        while num_next.next:
+            num_next = num_next.next
+        num_next.next = new_node
+    
+    def remove_num(self, num):
+        pass
+        
+    def display_list(self):
+        list_num =[]
+        num = self.head
+        while num is not None:
+            list_num.append(num.data)
+            num = num.next
+        print(list_num)
+
+if __name__ == '__main__':
+    list_2 = Node(2)
+    list_3 = Node(3)
+    list_1 = linked_list()
+    list_1.head = Node(1)
+    list_1.head.next = list_2
+    list_2.next = list_3
+    list_1.append_begin_list(0)
+    list_1.append_begin_list(-1)
+    list_1.append_end_list(4)
+    list_1.append_end_list(5)
+    list_1.display_list()
