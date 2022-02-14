a0746d72f1056def62b50865886ebd6cd614357f
DuyNguyen <drduyvn555@gmail.com>
2022-02-14 21:13:05 +0700

    Update check_button.py

--- python_basic/12.library_tkinter/check_button.py
+++ python_basic/12.library_tkinter/check_button.py
@@ -10,7 +10,7 @@ if __name__ == '__main__':
     check = Tk()
     check.geometry("100x100")
 
-    button_1 = Button(check, text="1", width=10, command=button_1_click).grid(padx= 10, pady=10)
+    button_1 = Button(check, text="1", width=10, command=button_1_click).grid(padx=10, pady=10)
     button_2 = Button(check, text="2", width=10, command=button_2_click).grid(padx=10, pady=10)
     
     # Execute
