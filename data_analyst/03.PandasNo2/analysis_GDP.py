ab272ab9fd5fb21889f9f78a55dbe967308342e9
DuyNguyen555 <drduyvn555@gmail.com>
2022-04-23 18:15:37 +0700

    update

--- data_analyst/03.PandasNo2/analysis_GDP.py
+++ data_analyst/03.PandasNo2/analysis_GDP.py
@@ -3,10 +3,10 @@ import numpy as np
 
 if __name__ == '__main__':
     df = pd.read_csv("03.PandasNo2\data\GDPlist.csv", )
-    # print("Số dòng là:", df.shape[0], "\n Số cột là: ", df.shape[1])
-    # df_gdp = df.loc[:, "GDP (millions of US$)"]
-    # print("Giá trị lớn nhất của GDP: ", df_gdp.max())
-    # print("Giá trị nhỏ nhất của GDP: ", df_gdp.min())
+    print("Số dòng là:", df.shape[0], "\n Số cột là: ", df.shape[1])
+    df_gdp = df.loc[:, "GDP (millions of US$)"]
+    print("Giá trị lớn nhất của GDP: ", df_gdp.max())
+    print("Giá trị nhỏ nhất của GDP: ", df_gdp.min())
     # print("Xu hướng phân bố GDP: ", df_gdp.mode())
     
     # df_chauluc = df.loc[:, 'Continent']
