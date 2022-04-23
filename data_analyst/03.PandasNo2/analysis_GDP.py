95290dfc7aec4198e7f1eb08913ae8f4fedd5f74
DuyNguyen555 <drduyvn555@gmail.com>
2022-04-23 19:03:29 +0700

    update

--- data_analyst/03.PandasNo2/analysis_GDP.py
+++ data_analyst/03.PandasNo2/analysis_GDP.py
@@ -7,14 +7,14 @@ if __name__ == '__main__':
     df_gdp = df.loc[:, "GDP (millions of US$)"]
     print("Giá trị lớn nhất của GDP: ", df_gdp.max())
     print("Giá trị nhỏ nhất của GDP: ", df_gdp.min())
-    # print("Xu hướng phân bố GDP: ", df_gdp.mode())
+    print("Xu hướng phân bố GDP: ", df_gdp.mode())
     
-    # df_chauluc = df.loc[:, 'Continent']
+    df_chauluc = df.loc[:, 'Continent']
     # print("Châu lục xuất hiện nhiều nhất là:", df_chauluc.mode())
     
     df_3 = df.loc[:,['Continent', "GDP (millions of US$)"]]    
-    # df_sum = df.pivot_table(values="GDP (millions of US$)", index="Continent", aggfunc=np.sum)
-    # df_av = df.pivot_table(values="GDP (millions of US$)", index="Continent", aggfunc="mean")
+    df_sum = df.pivot_table(values="GDP (millions of US$)", index="Continent", aggfunc=np.sum)
+    df_av = df.pivot_table(values="GDP (millions of US$)", index="Continent", aggfunc="mean")
     
-    # df2 = df.merge(df_sum, df_av, on="Continent")
-    # print(df2)
+    df2 = df.merge(df_sum, df_av, on="Continent")
+    print(df2)
