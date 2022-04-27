250c0082b5c89e163159a9943acf2ad935be0cae
DuyNguyen555 <drduyvn555@gmail.com>
2022-04-27 14:45:36 +0700

    Update demo2.py

--- data_analyst/04.DataManipulation/demo2.py
+++ data_analyst/04.DataManipulation/demo2.py
@@ -2,7 +2,8 @@ import pandas as pd
 
 if __name__ == '__main__':
     df = pd.read_csv("04.DataManipulation\data\shopeep_koreantop_clothing_shop_data.csv")
+    # print(df.shape)
     
     df2 = df.groupby(['name'])['follower_count'].mean()
-    df3 = df.groupby(['name'])['rating_normal'].sum()
     print(df2)
+    df3 = df.groupby(['name'])['rating_normal'].sum()
