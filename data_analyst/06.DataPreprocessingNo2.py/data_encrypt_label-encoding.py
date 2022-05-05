caffd563bbd2fcd949c8b214e906b91b5c2ac0d4
DuyNguyen555 <drduyvn555@gmail.com>
2022-05-05 21:06:36 +0700

    Update data_encrypt_label-encoding.py

--- data_analyst/06.DataPreprocessingNo2.py/data_encrypt_label-encoding.py
+++ data_analyst/06.DataPreprocessingNo2.py/data_encrypt_label-encoding.py
@@ -6,9 +6,16 @@ if __name__ == '__main__':
     # Mã hóa bằng sklearn
     encoder = LabelEncoder()
     lb = encoder.fit_transform(['red', 'red', 'yellow', 'green', 'yellow'])
-    print(lb)
+    # print(lb)
     
-    # Mã hóa bằng python
+    # Mã hóa bằng pandas
     s = pd.Series(['red', 'red', 'yellow', 'green', 'yellow'])
-    print(s.astype('category').cat.codes)
+    # print(s.astype('category').cat.codes)
     
+    data = ["red","yellow","green","red"]
+    label_enc = LabelEncoder()
+    label_enc = label_enc.fit(data)
+
+    transform = label_enc.transform(data)
+
+    print(transform)
