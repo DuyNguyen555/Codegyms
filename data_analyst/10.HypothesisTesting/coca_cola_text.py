0203a7b2825532ad61abe8efe7b833d59188870e
DuyNguyen555 <drduyvn555@gmail.com>
2022-06-09 08:12:22 +0700

    Update coca_cola_text.py

--- data_analyst/10.HypothesisTesting/coca_cola_text.py
+++ data_analyst/10.HypothesisTesting/coca_cola_text.py
@@ -4,13 +4,13 @@ import matplotlib.pyplot as plt
 
 if __name__ == '__main__':
     df = pd.read_excel('10.HypothesisTesting\data\Coca_cola_use.xlsx')
-    print(df.describe())
+    # print(df.describe())
     
     # Tiến hành kiểm định
     # a1: Ohio
     # a2: Atlanta
     # Giả thuyết H0: a1 - a2 = 0
     # Giả thuyết H1 a1 - a2 > 0
-    print(stats.ttest_ind(df['Ohio'], df['Atlanta'], equal_var=False))
+    print(stats.ttest_ind(df['Ohio'], df['Atlanta'], equal_var=False, alternative='greater'))
     # statistic>0, pvalue>0.05 ==> Chấp nhận H0 và bác bỏ H1
     # ==> Kết luận: Không đủ căn cứ để kết luận rằng lượng tiêu thụ coca trung bình ở Ohio lớn hơn ở Atlanta
