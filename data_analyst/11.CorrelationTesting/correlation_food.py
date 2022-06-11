f49902d9077f7ec623abc09751f2da55a4a1b37a
DuyNguyen555 <drduyvn555@gmail.com>
2022-06-11 14:29:38 +0700

    Update correlation_food.py

--- data_analyst/11.CorrelationTesting/correlation_food.py
+++ data_analyst/11.CorrelationTesting/correlation_food.py
@@ -3,22 +3,24 @@ import seaborn as sns
 from scipy import stats
 import matplotlib.pyplot as plt
 
+# Giữa giá gạo ở National Average và thời gian có mối liên hệ với nhau hay không
 if __name__ == '__main__':
     df = pd.read_csv('11.CorrelationTesting\data\FoodPrice_in_Turkey.csv')
-
+    print(df.describe())
+    
     # lọc dữ liệu gạo bán lẻ
     df_rice = df[(df.ProductName == "Rice - Retail") & (df.Place == "National Average")]
-    df_rice['time'] = pd.to_datetime(df_rice['Year'].astype(str) + "/" + df_rice["Month"].astype(str))
-
-    sns.set(style='darkgrid', palette='twilight')
-    plt.plot(df_rice['time'], df_rice['Price'], 'b-*', lw=2, ms=10, mfc='r')
-    plt.xlabel('Time', fontsize=14)
-    plt.ylabel('Price', fontsize=14)
-
-    plt.show()
-    
+    df_rice['time'] = pd.to_datetime(df['Year'].astype(str) + "/" + df['Month'].astype(str))
     df_rice['time_processed'] = df_rice.Month + (df_rice.Year - 2013)*12
-    plt.plot(df_rice['time_processed'], df_rice['Price'], 'c-*', lw=2, ms=12, mfc='r', mec='k')
-    plt.show()
     
-    print("Hệ số tương quan và pvalue tương ứng là:\n", stats.pearsonr(df_rice.time_processed, df_rice.Price))
+    # H0: r = 0
+    # H1: r # 0
+    print("Hệ số tương quan và pvalue tương ứng là:\n", stats.pearsonr(df_rice['time_processed'], df_rice['Price']))
+    # Vì pvalue <, hệ số tương quan=0.88 nên giữa giá gạo và thời gian có tương quan với nhau
+    
+    sns.set(style='darkgrid')
+    fig, ax = plt.subplots(2,1)
+    ax[0].plot(df_rice['time'], df_rice['Price'], 'c-*', lw=3, ms=11, mfc='r', mew=1, mec='b')
+    ax[1].plot(df_rice['time_processed'], df_rice['Price'], 'm-*', lw=3, ms=11, mfc='r', mew=1, mec='k')
+    
+    plt.show()
